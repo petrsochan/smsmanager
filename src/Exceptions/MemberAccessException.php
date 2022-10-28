80240febe25de0055b171d5d38df8b06766c0cd1
Petr Socháň <petr.sochan@gmail.com>
2022-10-28 15:38:46 +0200

    autoload

---
+++ src/Exceptions/MemberAccessException.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+namespace Pes\SmsManager;
+
+class MemberAccessException extends \LogicException
+{
+}
---
+++ src/Exceptions/NotImplementedException.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+namespace Pes\SmsManager;
+
+class NotImplementedException extends \LogicException
+{
+}
